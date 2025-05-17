3c07bfe577b96a83cc048289a6ba02f9742c2105
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2025-05-17 22:55:32 +0100

    Integrate Features section into App component and enhance Hero section text for clarity

---
+++ src/constants/index.jsx
@@ -0,0 +1,47 @@
+export const features = [
+  {
+    id: "0",
+    icon: "/images/feature-1.png",
+    caption: "Seamless Setup",
+    title: "Smarter Work, Less Effort",
+    text: "With NovaUI, tedious tasks are history. Automation and intelligent design elevate your productivity to new heights. It’s like an extra shot of focus—no caffeine required.",
+    button: {
+      icon: "/images/magictouch.svg",
+      title: "Watch the demo",
+    },
+  },
+  {
+    id: "1",
+    icon: "/images/feature-2.png",
+    caption: "Secure & reliable",
+    title: "Sleep easy, we got your back",
+    text: "Your data’s safety is our mission. With advanced encryption and powerful privacy features, NovaUI keeps your information secure and locked up tighter than Fort Knox.",
+    button: {
+      icon: "/images/docs.svg",
+      title: "Read the docs",
+    },
+  },
+];
+
+export const details = [
+  {
+    id: "0",
+    icon: "/images/detail-1.png",
+    title: "AI automated video editing",
+  },
+  {
+    id: "1",
+    icon: "/images/detail-2.png",
+    title: "Collaborate with your team",
+  },
+  {
+    id: "2",
+    icon: "/images/detail-3.png",
+    title: "Ultra fast cloud-engine",
+  },
+  {
+    id: "3",
+    icon: "/images/detail-4.png",
+    title: "24 / 7 Customer support",
+  },
+];
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,10 +8,10 @@ const Hero = () => {
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
             <div className="caption small-2 uppercase text-p3">
-              Image Editing
+              AI-powered image editing
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
-              Amazingly simple
+              Incredibly easy
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
               NovaUI is an AI Image Editor to be an easy to use, quick to learn,
